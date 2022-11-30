06c7a22c60b57c1057287da88a078ff02b582b79
limou3434 <898738804@qq.com>
2022-11-30 15:23:26 +0800

    学校实验代码作业2

---
+++ 实验代码2（备好）/实验2/实验2/实验2.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<stdlib.h>
+//任务一
+//int main()
+//{
+//	int var1;
+//	double var2;
+//	char var3;
+//	scanf("%d,%lf,%c", &var1, &var2, &var3);
+//	printf("var1=%d\n", var1);
+//	printf("var2=%lf\n",var2);
+//	printf("var3=%c\n", var3);
+//	return 0;
+//}
+//任务二
+//int main()
+//{
+//	int x = 010, y = 10, z = 0x10;
+//	char  c1='M', c2='\x4d', c3='\115', c4=77, c;
+//	printf("x=%o,y=%d,z=%x\n",x,y,z);
+//	printf("x=%d,y=%d,z=%d\n", x, y, z);
+//
+//	printf("c1=%c,c2=%x,c3=%o,c4=%d\n", c1, c2, c3, c4);
+//	printf("c1=%c,c2=%c,c3=%c,c4=%c\n", c1, c2, c3, c4);
+//	printf("c1=%d,c2=%d,c3=%d,c4=%d\n", c1, c2, c3, c4);
+//
+//	c = c1 + 32;
+//	printf("c=%c,c=%d",c,c);
+//	return 0;
+//}
+//任务三
+//int main()
+//{
+//	float grade1, grade2, grade3;
+//	float sum;
+//	printf("输入第一个学生三门课成绩（用空格分割）：\n");
+//	scanf("%f %f %f", &grade1, &grade2, &grade3);
+//	getchar();
+//	sum = grade1 + grade2 + grade3;
+//	printf("第一个学生三门课的总成绩=%f\n", sum);
+//
+//
+//	printf("输入第二个学生三门课成绩（用逗号分割）：\n");
+//	scanf("%f,%f,%f", &grade1, &grade2, &grade3);
+//	getchar();
+//	sum = grade1 + grade2 + grade3;
+//	printf("第二个学生三门课的总成绩=%f\n", sum);
+//
+//
+//	printf("输入第三个学生三门课成绩（用冒号分割）：\n");
+//	scanf("%f:%f:%f", &grade1, &grade2, &grade3);
+//	getchar();
+//	sum = grade1 + grade2 + grade3;
+//	printf("第三个学生三门课的总成绩=%f\n", sum);
+//	return 0;
+//}
