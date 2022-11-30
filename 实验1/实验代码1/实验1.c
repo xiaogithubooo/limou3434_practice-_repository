df0c5cd6f076b82acb670f6d37a5d11fcd1e1fd4
limou3434 <898738804@qq.com>
2022-11-30 15:17:33 +0800

    学校实验代码作业1

---
+++ 实验1/实验代码1/实验1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<Windows.h>
+/*int main()
+{
+	int i = -20;
+	unsigned int j = 10;
+	printf("%d\n", i + j);
+	return 0;
+}*/
+/*int main()
+{
+	unsigned int i;
+	for (i = 9; i >= 0; i--)
+	{
+		printf("%u\n", i);
+		Sleep(100);
+	}
+	return 0;
+}*/
+int main()
+{
+	char a [6] = { 1,1,1,2,3 };
+	printf("%d", strlen(a));
+	return 0;
+}
