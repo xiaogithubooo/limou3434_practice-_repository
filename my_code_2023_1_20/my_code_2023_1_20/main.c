13c1f77a6e31a816dd1698c772f55ce4d603642e
limou3434 <898738804@qq.com>
2023-01-21 00:23:26 +0800

    测试作业代码

---
+++ my_code_2023_1_20/my_code_2023_1_20/main.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+////1°¢≤‚ ‘¥˙¬Î
+//int main()
+//{
+//	char c = 'A';
+//	if ('0' <= c <= '9') printf("YES");
+//	else printf("NO");
+//
+//	printf("\n");
+//
+//	return 0;
+//}
+////2°¢≤‚ ‘¥˙¬Î
+//int main()
+//{
+//	float a[3] = { 1.5,2.5,3.5 }, * pa = a;
+//	*(pa++) *= 3;
+//	printf("%f", *pa);
+//	return 0;
+//}
+////3°¢≤‚ ‘¥˙¬Î
+//int main()
+//{
+//	int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, * p = a + 5, * q = NULL;
+//	*q = *(p + 5);
+//	printf("%d %d\n", *p, *q);
+//	return 0;
+//}
+////4°¢≤‚ ‘¥˙¬Î
+//int main()
+//{
+//	char* p[] = { "Shanghai","Beijing","Honkong" };
+//	printf("%c", *(p[1] + 3));
+//	return 0;
+//}
+////5°¢≤‚ ‘¥˙¬Î
+//struct T
+//{
+//	char name[20];
+//	int age;
+//	int sex;
+//} a[5], * pa = a;
+//int main()
+//{
+//	
+//	return 0;
+//}
+////6°¢≤‚ ‘¥˙¬Î
+//int main()
+//{
+//	unsigned char i = 7;
+//	int j = 0;
+//	for (; i > 0; i -= 3)
+//	{
+//		++j;
+//	}
+//	printf("%d\n", j);
+//	return 0;
+//}
