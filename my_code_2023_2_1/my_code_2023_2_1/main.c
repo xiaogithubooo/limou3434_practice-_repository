112d0158a8c6e30b2d5366087480299fd659c795
limou3434 <898738804@qq.com>
2023-02-01 18:16:55 +0800

    复合字面量和指针的实验

---
+++ my_code_2023_2_1/my_code_2023_2_1/main.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+////1、实验代码
+//int main()
+//{
+//	int a = 1;
+//	int **ppa;
+//	int* pa;
+//	pa = (&a);
+//	ppa = (&pa);
+//
+//
+//	return 0;
+//}
+////2、复合字面量的使用
+//int main() 
+//{
+//	int arr[3] = { 1, 2, 3 };
+//	(int [3]){ 1,2,3 };
+//
+//	int *pt1;
+//	pt1 = (int[2]){ 10,20 }; 
+//
+//	int(*parr_1)[3];
+//	parr_1 = (int[2][3]){ {1,2,3},{2,3,4} };
+//	return 0;
+//}
