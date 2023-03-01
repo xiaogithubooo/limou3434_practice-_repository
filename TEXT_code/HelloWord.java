025acd866a67e21ccefcef3f07d1700a863691c6
limou3434 <898738804@qq.com>
2023-03-01 14:57:18 +0800

    初步学习Java，使用了文档加命令行的方式和直接使用IDEA的方式运行和编写第一个Java程序

---
+++ TEXT_code/HelloWord.java
@@ -0,0 +1,17 @@
+public class HelloWord {
+	public static void main(String[] args){
+		System.out.println("hello");
+	}
+}
+
+class HelloWord_1 {
+	public static void main(String[] args){
+		System.out.println("word");
+	}
+}
+
+class HelloWord_2 {
+	public static void main(String[] args){
+		System.out.println("I'am limou3434.");
+	}
+}
---
+++ my_java_2023_3_1/src/Main.java
@@ -0,0 +1,36 @@
+//public class Main {
+//    //psvm
+//    public static void main(String[] args) {
+//        //sout
+//        System.out.println();
+//        //10.sout
+//        System.out.println(10);
+//        //"hello".sout
+//        System.out.println("hello");
+//        //fori
+//        for (int i = 0; i <; i++) {
+//
+//        }
+//        //10.fori
+//        for (int i = 0; i < 10; i++) {
+//
+//        }
+//    }
+//}
+public class Main {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
+
+class Main_1 {
+    public static void main(String[] args){
+        System.out.println("word");
+    }
+}
+
+class Main_2 {
+    public static void main(String[] args){
+        System.out.println("I'am limou3434.");
+    }
+}
