0dc8b8442a6602685f10d86e895507d873bad2f3
limou3434 <898738804@qq.com>
2023-03-02 23:25:26 +0800

    一个测试Java标识符命名规则的简单Java代码

---
+++ my_java_2023_3_2/src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+        int a = 10;
+        int __________________ = 10;
+        System.out.print(__________________);
+    public static void main_1(String[] args) {
+        int $name = 100;
+    }
+    public static void main(String[] args) {
+    int $name = 100;
+    }
+}
+/*测试奇葩的标识符命名规则*/
