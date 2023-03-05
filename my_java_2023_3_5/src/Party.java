0a50f43a1d8e3842b99e683ff4a2feb065365924
limou3434 <898738804@qq.com>
2023-03-05 11:14:23 +0800

    测试一个Java代码

---
+++ my_java_2023_3_5/src/Party.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+class Party{
+    public void buildlnvite(){
+        Frame f = new Frame();
+        Label l = new Label("Party at Tim's");
+        Button b = new Button("You bet");
+        Button c = new Button("Shoot me");
+        Panel p = new Panel();
+        p.add(l);
+    }//还是不能运行，可能有外部的东西没有引入
+}
