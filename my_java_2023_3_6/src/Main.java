56293b50e398c80269b57ece8b80e984096b10fa
limou3434 <898738804@qq.com>
2023-03-06 23:44:06 +0800

    尝试使用IDEA环境运行Java代码，还有使用了Java的数据类型和变量，以及初步了解包装类

---
+++ my_java_2023_3_6/src/Main.java
@@ -0,0 +1,24 @@
+/*
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE+" "+Integer.MIN_VALUE);//Integer是包装类,简单理解为int的puls版本
+        int a = 10;
+        double d = 3.1415926;
+        char c = 'A';
+        boolean b = true;
+        System.out.println(a+" "+d+" "+c+" "+b);
+    }
+}*/
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println(a / b);
+        double c = 1.0;
+        double d = 2.0;
+        System.out.println(c / d);
+        double e = 1.0;
+        int f = 2;
+        System.out.println(e / f);
+    }
+}
