2792706de0cceee23e64286975c46e6d953ec3d1
limou3434 <898738804@qq.com>
2023-03-09 11:12:44 +0800

    测试使用了Java的运算符和类型之间的转换

---
+++ my_java_2023_3_9/src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.text.Style;
+
+public class Main {
+    public static void main(String[] args) {
+        char a = 10;
+        char b = 20;
+        //char c = a + b;
+    }
+}
+class Main1 {
+    public static void main(String[] args) {
+        double a =10.0;
+        a /= 3.14;
+        System.out.println(a);
+    }
+}
