d54de989cdc34065f9dec1e514b6277127afc760
limou3434 <898738804@qq.com>
2023-03-10 13:57:29 +0800

    PTA的一道题目

---
+++ my_code_2023_3_10/my_code_2023_3_10/main.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+////PTA题目：逆序的三位数，https://pintia.cn/problem-sets/14/exam/problems/783
+//void function(int number)
+//{
+//	static int k = 0;
+//	if (number % 10 || k)
+//	{
+//		printf("%d", number % 10);
+//		k = 1;
+//	}
+//	if (number >= 10)
+//	{
+//		function(number / 10);
+//	}
+//}
+//int main()
+//{
+//	int number = 0;
+//	scanf("%d", &number);
+//	function(number);
+//	return 0;
+//}
+////2、
