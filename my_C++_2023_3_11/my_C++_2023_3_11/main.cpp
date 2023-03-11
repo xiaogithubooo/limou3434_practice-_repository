0b670decddfa383282fd7527968601d75f66f870
limou3434 <898738804@qq.com>
2023-03-11 23:28:57 +0800

    日志： 尝试使用第一个C++代码

---
+++ my_C++_2023_3_11/my_C++_2023_3_11/main.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//日志
+//尝试使用第一个C++代码
+#include <iostream>
+int main()
+{
+	using namespace std;//编译指令
+	cout << "Hello C++!";//输出到标准输出
+	cout << endl;//换行
+	cout << "Hello C++!!!" << endl;//输出到标准输出，并且换行
+	return 0;
+}
