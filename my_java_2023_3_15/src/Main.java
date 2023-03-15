2c8981c54bd5acf20b822c86258e88a9ef789f25
limou3434 <898738804@qq.com>
2023-03-15 13:37:59 +0800

    日志：Java控制语句中的分支语句学习

---
+++ my_java_2023_3_15/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int x = 20;
+        if(x == 100);{
+            System.out.println("hello");
+        }
+    }
+}
+class Main1 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 100;
+        if (x == 10)
+            if (y == 10)
+                System.out.println("1");
+        else
+            System.out.println("0");
+    }
+}
+class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);//代表从键盘读入数据
+        int a = scanner.nextInt();
+        System.out.println(a);
+    }
+}
