0d934e439fcfaf78f7499baa9bf801609d404dea
limou3434 <898738804@qq.com>
2023-03-16 13:40:23 +0800

    日志：学习了Java的循环控制语句，并且了解了switch的细节

---
+++ my_java_2023_3_16/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        long a = 10;
+        switch(a){
+            case 10:
+                System.out.println("Hello world!");
+                break;
+        }
+    }
+}
