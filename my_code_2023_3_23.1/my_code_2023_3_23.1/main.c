f3d581041ef2bf452e5c661bf12c842f08910013
limou3434 <898738804@qq.com>
2023-03-23 10:53:45 +0800

    日志：PTA的两道简单题目

---
+++ my_code_2023_3_23.1/my_code_2023_3_23.1/main.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+////1、厘米换算英尺英寸：https://pintia.cn/problem-sets/14/exam/problems/781
+#include <stdio.h>
+//int main()
+//{
+//    double number = 0;
+//    scanf("%lf", &number);
+//    number /= 100;//转化为米数
+//    double a = number / 0.3048;
+//    int b = number / 0.3048;
+//    int c = (a - b) * 12;
+//    printf("%d %d", b, c);
+//    return 0;
+//}
+////2、表格输出：https://pintia.cn/problem-sets/14/exam/problems/785
+//int main()
+//{
+//    printf("------------------------------------\n"
+//        "Province      Area(km2)   Pop.(10K)\n"
+//        "------------------------------------\n"
+//        "Anhui         139600.00   6461.00\n"
+//        "Beijing        16410.54   1180.70\n"
+//        "Chongqing      82400.00   3144.23\n"
+//        "Shanghai        6340.50   1360.26\n"
+//        "Zhejiang      101800.00   4894.00\n"
+//        "------------------------------------");
+//    return 0;
+//}
