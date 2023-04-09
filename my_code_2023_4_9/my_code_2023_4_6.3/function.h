900870a702ca5c9893c2ab5dac58d827b388cc20
limou3434 <898738804@qq.com>
2023-04-09 13:23:26 +0800

    日志：学习了C语言快排的优化 （1）小区间优化 （2）三数取中优化

---
+++ my_code_2023_4_9/my_code_2023_4_6.3/function.h
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 1000
+#define SCOPE 1000
+
+//1.≤Â»Î≈≈–Ú
+//1.1.÷±Ω”≤Â»Î≈≈–Ú
+void InsertSort(int* a, int n);
+//1.2.œ£∂˚≈≈–Ú
+void ShellSort(int* a, int n);
+
+//2.—°‘Ò≈≈–Ú
+//2.1.÷±Ω”—°‘Ò≈≈–Ú
+void SelectSort(int* a, int n);
+//2.2.∂—≈≈–Ú
+void HeapSort(int* a, int n);
+
+//3.Ωªªª≈≈–Ú
+//3.1.√∞≈›≈≈–Ú
+void BubbleSort(int* a, int n);
+//3.2.øÏÀŸ≈≈–Ú
+//3.2.1.Õ⁄ø”∑®
+void QuickSort(int* a, int left_begin, int right_end);
+
+
+//4.πÈ≤¢≈≈–Ú+
+//4.1.πÈ≤¢≈≈–Ú
