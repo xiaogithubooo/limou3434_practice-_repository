eac61624c4acca5c435344be8da1e448300a600a
limou3434 <898738804@qq.com>
2023-05-01 17:07:03 +0800

    日志：提交一份排序算法准备代码，以供日后使用

---
+++ my_code_2023_5_1.1/my_code_2023_5_1.1/main.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//# 0.复习目标
+//>		0.1.排序算法代码复习
+//			0.1.1.课堂代码复习
+//			0.1.2.手撕插入排序
+//				0.1.2.1.直接插入排序
+//				0.1.2.2.希尔排序
+//			0.1.3.手撕选择排序
+//				0.1.3.1.直接选择排序
+//				0.1.3.2.堆排序
+//			0.1.4.手撕归并排序
+//				0.1.4.1.归并排序
+//>		0.2.排序算法实践
+//			0.2.1.排序数组：https://leetcode.cn/problems/sort-an-array/
+//			0.2.2.使每位学生都有座位的最少移动次数：https://leetcode.cn/problems/minimum-number-of-moves-to-seat-everyone/
+//			0.2.3.拆分数位后四位数字的最小和：https://leetcode.cn/problems/minimum-sum-of-four-digit-number-after-splitting-digits/
+//			0.2.4.删除每行中的最大值：https://leetcode.cn/problems/delete-greatest-value-in-each-row/
+//			0.2.5.有多少小于当前数字的数字：https://leetcode.cn/problems/how-many-numbers-are-smaller-than-the-current-number/
+//>		0.3.其他题目练习
+//
