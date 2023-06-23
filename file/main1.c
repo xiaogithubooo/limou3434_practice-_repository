2f62e196d93368fb1af6517582ad33902cf91387
dimou <1346965749@qq.com>
2023-06-23 11:21:49 +0800

    Commit test in Ubuntu

---
+++ file/main1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    printf("Hello, This is a C code!\n");
+    for(i; i < 10; i++)
+    {
+        printf("%d ", i * i );
+    }
+    printf("\n");
+    return 0;
+}
