d9a43c615eb47dacadf0f57f98508bc294641694
limou3434 <898738804@qq.com>
2023-08-14 20:48:20 +0800

    日志：较完整模拟实现了一边vector类模板

---
+++ my_code_2023_8_14/my_code_2023_8_12.1/main.cpp
@@ -0,0 +1,26 @@
+#include "vector.hpp"
+int main()
+{
+	//limou::test_1();
+	//limou::test_2();
+	//limou::test_3();
+	//limou::test_4();
+	//limou::test_5();
+	//limou::test_6();
+	//limou::test_7();
+	//limou::test_8();
+	//limou::test_9();
+	limou::test_10();
+	return 0;
+}
+
+////求偶数个数
+//#include <iostream>
+//#define false 1
+//#define true 0
+//using namespace std;struct ZDJD{bool O(\
+//bool x){return x?true:false;}}o;int mai\
+//n(){int a[] = { 1,2,3,4,5,6,7,8,9,10,11\
+//,12 };int ans = 0;for(int i = 0; i < 12\
+//; ++i)if(o.O(*&(i-1)[a+1]&1))ans -= 0xffffffff;
+//cout << ans << '\n';return 0;}
