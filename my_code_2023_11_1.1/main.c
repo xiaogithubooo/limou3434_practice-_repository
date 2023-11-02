80d23bf8e1dbf8cb3f6f4266bc2f7f93be410cf2
limou3434 <898738804@qq.com>
2023-11-02 17:25:34 +0800

    日志：一些关于进程等待和终止的复习

--- my_code_2023_11_1.1/main.c
+++ my_code_2023_11_1.1/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -13,26 +15,85 @@ void Worker()
         sleep(1);
     }
 }
-
 int main()
 {
-    pid_t id = fork();
-    if(id == 0)
+    int n = 10;
+    int i = 0;
+
+    for(i = 0; i < n; i++)
     {
-        //child
-        Worker();
-        exit(0);
-    }
-    else
-    {
-        //father
-        sleep(10);//在后10秒子进程处于僵尸状态
-        pid_t rid = wait(NULL);
-        if(rid == id)
+        pid_t id = fork();
+        if(id == 0)
         {
-            printf("I am father process, wait success, pid: %d\n", getpid());
+            Worker();
+            exit(i);
+        }
+    }
+
+    //等待多个子进程
+    for(i = 0; i < n; i++)
+    {
+        int status = 0;
+        pid_t rid = waitpid(-1, &status, 0);
+        if(rid > 0)
+        {
+            printf("wait child %d success, exit code: %d\n", rid, WEXITSTATUS(status));
         }
     }
 
     return 0;
 }
+
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <errno.h>
+// #include <unistd.h>
+// #include <sys/types.h>
+// #include <sys/wait.h>
+
+// void Worker()
+// {
+//     int cnt = 5;
+//     while(cnt)
+//     {
+//         printf("I am child process, pid: %d, ppid: %d, cnt: %d\n", getpid(), getppid(), cnt--);
+//         sleep(1);
+//     }
+// }
+
+// int main()
+// {
+//     pid_t id = fork();
+//     if(id == 0)
+//     {
+//         //child
+//         Worker();
+
+//         //下面这两句可以测试进程因为异常退出的情况
+//         //int a = 10;
+//         //a /= 0;
+
+//         exit(10);
+//     }
+//     else
+//     {
+//         //father
+//         int status = 0;
+//         pid_t rid = waitpid(id, &status, 0);//和使用 wait() 是一样的
+//         if(rid == id)
+//         {
+//             if(WIFEXITED(status))//如果进程正常终止，返回真
+//             {
+//                 printf("child process normal quit, exit code: %d\n", WEXITSTATUS(status));
+//             }
+//             else
+//             {
+//                 printf("child process quit except\n");
+//             }
+//         }
+//     }
+
+//     return 0;
+// }
