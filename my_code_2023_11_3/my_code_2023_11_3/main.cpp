080c16f6206ffdbb28973f32bdc3b84c036cf114
limou3434 <898738804@qq.com>
2023-11-03 14:09:57 +0800

    日志：虚继承和棱形继承和多态分析

--- my_code_2023_11_3/my_code_2023_11_3/main.cpp
+++ my_code_2023_11_3/my_code_2023_11_3/main.cpp
@@ -1,125 +1,156 @@
-//打印虚函数表地址和虚函数地址，以及虚函数表地址和其他区域地址的对比
 #include <iostream>
 using namespace std;
 
-class Father
-{
-public:
-	virtual void Function_1()
-	{
-		cout << "Father:Function_1()" << endl;
-	}
-	virtual void Function_2()
-	{
-		cout << "Father:Function_2()" << endl;
-	}
-private:
-	int _b_number = 1;
+class A {
+public: virtual void func1() { cout << "A::func1" << endl; }
+public:	int _a = 1;
 };
 
-class Son : public Father
-{
-public:
-	virtual void Function_1()
-	{
-		cout << "Son:Function_1()" << endl;
-	}
-	virtual void Function_3()
-	{
-		cout << "Son:Function_3()" << endl;
-	}
-	virtual void Function_4()
-	{
-		cout << "Son:Function_4()" << endl;
-	}
-	void Function_5()
-	{
-		cout << "Son:Function_5()" << endl;
-	}
-private:
-	int _d_number = 2;
+class B : virtual public A {
+public:	virtual void func1() { cout << "B::func1" << endl; }
+public:	virtual void func3() { cout << "B::func3" << endl; }
+public:	int _b = 2;
 };
 
-class Grandson : public Son
-{
-public:
-	virtual void Function_3()
-	{
-		cout << "Grandson:Function_3()" << endl;
-	}
+class C : virtual public A {
+public:	virtual void func1() { cout << "C::func1" << endl; }
+public:	virtual void func4() { cout << "C::func4" << endl; }
+public:	int _c = 3;
 };
 
-void Function(Father* f)
-{
-	f->Function_1();
-}
+class D : public B, public C {
+public:	virtual void func1() { cout << "D::func1" << endl; }
+public:	virtual void func2() { cout << "D::func2" << endl; }
+public:	int _c = 4;
+};
 
-void test_1()
-{
-	Father f;
-	Son s;
-	Grandson g;
-
-	static int a = 0;
-	int b = 0;
-	int* p1 = new int;
-	const char* p2 = "hello";
-
-	printf("静态区：%p\n", &a);
-	printf("栈区：%p\n", &b);
-	printf("堆区：%p\n", p1);
-	printf("代码段/常量区：%p\n", p2);
-	printf("虚表地址：%p\n", *((int*)(&s)));
-
-	printf("Father::Function_1 虚函数地址：%p\n", &Father::Function_1);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
-	printf("Father::Function_2 虚函数地址：%p\n", &Father::Function_2);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
-	printf("Son::Function_1 虚函数地址：%p\n", &Son::Function_1);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
-	printf("Son::Function_2 虚函数地址：%p\n", &Son::Function_2);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
-	printf("Son::Function_3 虚函数地址：%p\n", &Son::Function_3);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
-	printf("Son::Function_4 虚函数地址：%p\n", &Son::Function_4);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
-	printf("Grandson::Function_1 虚函数地址：%p\n", &Grandson::Function_1);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
-	printf("Grandson::Function_2 虚函数地址：%p\n", &Grandson::Function_2);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
-	printf("Grandson::Function_3 虚函数地址：%p\n", &Grandson::Function_3);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
-	printf("Grandson::Function_4 虚函数地址：%p\n", &Grandson::Function_4);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
-
-	printf("普通函数地址：%p\n\n", Function);
-	//注意：成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
-	//注意：不可以使用 bTest.Function_1 这种方式打印虚函数地址，因为函数地址没有被存储在对象中
-}
-
-typedef void(*VFUNC)();
-void PrintVFT(VFUNC arr[])//接受虚函数表指针，并且打印虚函数表内的函数地址内容，然后进行调用
-{
-	for (size_t i = 0; arr[i] != 0; i++)//这里是根据 VS 的特殊处理才可以这么写停止循环的条件的，其他环境可能只能自己写固定的循环次数
-	{
-		printf("_vfptr[%d]:%p->", i, arr[i]);
-		arr[i]();//调用函数
-	}
-	printf("\n");
-}
-void test_2()
-{
-	Father f;
-	VFUNC* vPtrT1 = (VFUNC*)(*(int*)&f);//虚函数表地址，如果是 64 位可以改 (int*) 为 (long long*)
-	PrintVFT(vPtrT1);
-
-	Son s;
-	VFUNC* vPtrT2 = (VFUNC*)(*(int*)&s);//虚函数表地址，如果是 64 位可以改 (int*) 为 (long long*)
-	PrintVFT(vPtrT2);
-
-	Grandson g;
-	VFUNC* vPtrT3 = (VFUNC*)(*(int*)&g);//虚函数表地址，如果是 64 位可以改 (int*) 为 (long long*)
-	PrintVFT(vPtrT3);
-}
-
-int main()
-{
-	//64位的输出会不一样（因为指针长度变成了 8 字节），建议换成32位平台再运行
-
-	test_1();
-	test_2();
+int main() {
+	D d;
 	return 0;
 }
+
+////打印虚函数表地址和虚函数地址，以及虚函数表地址和其他区域地址的对比
+//#include <iostream>
+//using namespace std;
+//
+//class Father
+//{
+//public:
+//	virtual void Function_1()
+//	{
+//		cout << "Father:Function_1()" << endl;
+//	}
+//	virtual void Function_2()
+//	{
+//		cout << "Father:Function_2()" << endl;
+//	}
+//private:
+//	int _b_number = 1;
+//};
+//
+//class Son : public Father
+//{
+//public:
+//	virtual void Function_1()
+//	{
+//		cout << "Son:Function_1()" << endl;
+//	}
+//	virtual void Function_3()
+//	{
+//		cout << "Son:Function_3()" << endl;
+//	}
+//	virtual void Function_4()
+//	{
+//		cout << "Son:Function_4()" << endl;
+//	}
+//	void Function_5()
+//	{
+//		cout << "Son:Function_5()" << endl;
+//	}
+//private:
+//	int _d_number = 2;
+//};
+//
+//class Grandson : public Son
+//{
+//public:
+//	virtual void Function_3()
+//	{
+//		cout << "Grandson:Function_3()" << endl;
+//	}
+//};
+//
+//void Function(Father* f)
+//{
+//	f->Function_1();
+//}
+//
+//void test_1()
+//{
+//	Father f;
+//	Son s;
+//	Grandson g;
+//
+//	static int a = 0;
+//	int b = 0;
+//	int* p1 = new int;
+//	const char* p2 = "hello";
+//
+//	printf("静态区：%p\n", &a);
+//	printf("栈区：%p\n", &b);
+//	printf("堆区：%p\n", p1);
+//	printf("代码段/常量区：%p\n", p2);
+//	printf("虚表地址：%p\n", *((int*)(&s)));
+//
+//	printf("Father::Function_1 虚函数地址：%p\n", &Father::Function_1);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
+//	printf("Father::Function_2 虚函数地址：%p\n", &Father::Function_2);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
+//	printf("Son::Function_1 虚函数地址：%p\n", &Son::Function_1);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
+//	printf("Son::Function_2 虚函数地址：%p\n", &Son::Function_2);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
+//	printf("Son::Function_3 虚函数地址：%p\n", &Son::Function_3);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
+//	printf("Son::Function_4 虚函数地址：%p\n", &Son::Function_4);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
+//	printf("Grandson::Function_1 虚函数地址：%p\n", &Grandson::Function_1);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
+//	printf("Grandson::Function_2 虚函数地址：%p\n", &Grandson::Function_2);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
+//	printf("Grandson::Function_3 虚函数地址：%p\n", &Grandson::Function_3);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
+//	printf("Grandson::Function_4 虚函数地址：%p\n", &Grandson::Function_4);//成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
+//
+//	printf("普通函数地址：%p\n\n", Function);
+//	//注意：成员函数这里有一个语法规定，不能只使用函数名，必须加上取地址
+//	//注意：不可以使用 bTest.Function_1 这种方式打印虚函数地址，因为函数地址没有被存储在对象中
+//}
+//
+//typedef void(*VFUNC)();
+//void PrintVFT(VFUNC arr[])//接受虚函数表指针，并且打印虚函数表内的函数地址内容，然后进行调用
+//{
+//	for (size_t i = 0; arr[i] != 0; i++)//这里是根据 VS 的特殊处理才可以这么写停止循环的条件的，其他环境可能只能自己写固定的循环次数
+//	{
+//		printf("_vfptr[%d]:%p->", i, arr[i]);
+//		arr[i]();//调用函数
+//	}
+//	printf("\n");
+//}
+//void test_2()
+//{
+//	Father f;
+//	VFUNC* vPtrT1 = (VFUNC*)(*(int*)&f);//虚函数表地址，如果是 64 位可以改 (int*) 为 (long long*)
+//	PrintVFT(vPtrT1);
+//
+//	Son s;
+//	VFUNC* vPtrT2 = (VFUNC*)(*(int*)&s);//虚函数表地址，如果是 64 位可以改 (int*) 为 (long long*)
+//	PrintVFT(vPtrT2);
+//
+//	Grandson g;
+//	VFUNC* vPtrT3 = (VFUNC*)(*(int*)&g);//虚函数表地址，如果是 64 位可以改 (int*) 为 (long long*)
+//	PrintVFT(vPtrT3);
+//}
+
+//int main()
+//{
+//	//64位的输出会不一样（因为指针长度变成了 8 字节），建议换成32位平台再运行
+//
+//	test_1();
+//	test_2();
+//	return 0;
+//}
 
 //#include <iostream>
 //using namespace std;
