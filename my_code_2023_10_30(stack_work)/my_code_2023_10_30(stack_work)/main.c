9a722de985eb372537c7ff0c3742362b3db38bfb
limou3434 <898738804@qq.com>
2023-11-06 08:45:15 +0800

    日志：作业代码提交

--- my_code_2023_10_30(stack_work)/my_code_2023_10_30(stack_work)/main.c
+++ my_code_2023_10_30(stack_work)/my_code_2023_10_30(stack_work)/main.c
@@ -1,13 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <math.h>
+#include <assert.h>
 #include "stack.h"
+#define SIZE 10000
 
 //1.测试栈的合理性
 void test();
 //2.十进制转化为N进制
-void 
+void conversion();
+
 int main()
 {
+	//1.栈测试接口
 	//test();
-	//
+
+	//2.进制转化
+	conversion();
 	return 0;
 }
 
@@ -32,4 +40,54 @@ void test()
 	//StackPop(&st1);//非法删除
 	//StackPop(&st1);//非法取栈顶元素
 	StackDestroy(&st1);//销毁栈结构体
+}
+
+//1.设置数码
+const char arr[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+void conversion()
+{
+	//2.输入数据
+	int M = 0;
+	int N = 0;
+	printf("输入数字和进制：");
+	int scanfRet = scanf("%d %d", &M, &N);//输入数字和数制
+	if (N <= 0)
+	{
+		printf("没有该进制，输入非法\n");
+		return 0;
+	}
+	char* number = (char*)malloc(sizeof(char) * SIZE);//最终结果存储的字符串
+	int flag = 1;
+	//3.提前处理负数和零的情况
+	if (M < 0)
+	{
+		flag = -1;
+		M = abs(M);
+	}
+	else if (M == 0)
+	{
+		printf("%d", 0);
+		return 0;
+	}
+
+	//4.进制转换
+	int i = 0;
+	while (M)
+	{
+		number[i++] = arr[M % N];//映射插入字符
+		M /= N;
+	}
+
+	//5.输出数据
+	if (flag == -1)
+	{
+		printf("-");
+	}
+	for (int j = i - 1; j >= 0; j--)
+	{
+		printf("%c", number[j]);
+	}
+
+	//6.释放内存
+	free(number);
 }
