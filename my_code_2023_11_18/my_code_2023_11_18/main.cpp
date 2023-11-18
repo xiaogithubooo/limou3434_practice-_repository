85a6d1074b4c3a9cc1a99d9587e1dcb227154943
limou3434 <898738804@qq.com>
2023-11-18 20:20:07 +0800

    日志：一些C++11语语的研究

--- my_code_2023_11_18/my_code_2023_11_18/main.cpp
+++ my_code_2023_11_18/my_code_2023_11_18/main.cpp
@@ -1,18 +1,15 @@
-#include <cstdio>
-#include <windows.h>
-
+#include <iostream>
 int main()
 {
-	int n = 0, ch;
-	ch = getchar();
-	while (ch != '\n')
-	{
-		if (ch >= '0' && ch <= '9')
-		{
-			n++;
-		}
-		ch = getchar();
-	}
-	printf("%d\n", n);
-	return 0;
+    const int x = 1;
+    double y = 2.3;
+
+    //decltype 可以根据表达式的类型来创建变量
+    decltype(x * y) ret;//ret的类型变成了 double
+    decltype(&x) p;//p的类型变成了 const int*
+
+    std::cout << typeid(ret).name() << '\n';
+    std::cout << typeid(p).name() << '\n';
+
+    return 0;
 }
