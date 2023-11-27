ea80528458773504c1e4d519640a6fe6e6b07339
limou3434 <898738804@qq.com>
2023-11-27 16:46:14 +0800

    脚本自动更新所有仓库

---
+++ my_code_2023_11_27/my_code_2023_11_27/main.cpp
@@ -0,0 +1,22 @@
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d\n", printf("0,1,2,3\n"));
+//	return 0; 
+//}
+//结果为
+//0，1，2，3
+//8
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int A = 43;
+//	printf("%d\n", printf("%d", printf("%d", A)));
+//}
+////结果会输出什么呢？432
+////第三个 printf 输出 "43" 字符数量为 2，于是返回值为 2，第二个 printf 就输出 "2"
+////第二个 printf 输出 "2" 字符数量为 1，于是返回值为 1，第一个 printf 就输出 "1"
