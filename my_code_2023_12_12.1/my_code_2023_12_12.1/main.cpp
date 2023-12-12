e25a14ab6dde49efd3d4686dc2853ada774a90a3
limou3434 <898738804@qq.com>
2023-12-12 20:28:42 +0800

    '12.12.脚本自动更新仓库limou-c-test-code'

--- my_code_2023_12_12.1/my_code_2023_12_12.1/main.cpp
+++ my_code_2023_12_12.1/my_code_2023_12_12.1/main.cpp
@@ -1,6 +1,4 @@
 #include <vector>
-#include <list>
-#include <utility>
 using namespace std;
 
 template <typename ArcType = int>
@@ -12,10 +10,11 @@ struct ArcBox
 	ArcBox* sameTail;			//相同尾的弧
 };
 
+
 template <typename VertexType = char, typename ArcType = int>
 struct VexNode
 {
-	VertexType data;			//顶点存储的数据
+	VertexType data;					//顶点存储的数据
 	ArcBox<ArcType>* firstHead;			//指向以该顶点为弧头的第一个顶点
 	ArcBox<ArcType>* firstTail;			//指向以该顶点为弧尾的第一个顶点
 };
@@ -25,8 +24,8 @@ class OLGraph
 {
 private:
 	vector<VexNode<VertexType, ArcType>> table;	//顶点表
-	int vexNum;							//图当前的顶点数
-	int arcNum;							//图当前的边数
+	int vexNum;									//图当前的顶点数
+	int arcNum;									//图当前的边数
 };
 
 
