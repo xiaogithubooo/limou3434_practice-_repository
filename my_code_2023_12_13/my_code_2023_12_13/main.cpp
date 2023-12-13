d15c869cb1bbb66ddfbf6d954a482f2ce4ebe6dd
limou3434 <898738804@qq.com>
2023-12-13 15:59:14 +0800

    12.13.脚本自动更新仓库limou-c-test-code

--- my_code_2023_12_13/my_code_2023_12_13/main.cpp
+++ my_code_2023_12_13/my_code_2023_12_13/main.cpp
@@ -4,7 +4,23 @@ using namespace std;
 using namespace limou;
 int main()
 {
-	string arr[3] = { "limou", "dimou", "iimou" };
-	UnionFindSet<string> ufs(arr, 3);
+	//{0}, {1}, {2}, {3}
+	UnionFindSet<> ufs(4);
+	cout << ufs.Count() << '\n';
+	if (ufs.InSet(1, 2))
+		cout << "OK" << '\n';
+
+	//{0, 1, 2}, {3}
+	ufs.Union(0, 1);
+	ufs.Union(1, 2);
+	cout << ufs.Count() << '\n';
+	if (ufs.InSet(1, 2))
+		cout << "OK" << '\n';
+
+	//{0, 1, 2} root->0
+	//{3} root->3
+	cout << ufs.FindRoot(2) << '\n';
+	cout << ufs.FindRoot(3) << '\n';
+
 	return 0;
 }
--- my_code_2023_12_13/my_code_2023_12_13/union_find_set.hpp
+++ my_code_2023_12_13/my_code_2023_12_13/union_find_set.hpp
@@ -1,23 +1,70 @@
 #pragma once
 #include <vector>
-#include <map>
 
 namespace limou
 {
-	template <typename Type>
+	template <typename Type = int>
 	class UnionFindSet
 	{
+		/*
+		* 这里的并查集只使用了索引来作为集合内的元素的编号
+		* 对应存储的是“根节点包含元素的个数”和“双亲结点索引”的信息
+		*/
+
 	public:
-		UnionFindSet(const Type* arr, size_t n)
+		UnionFindSet(int n)
+			: _ufs(n, -1)
+		{}
+
+		bool Union(int x1, int x2)
 		{
-			for (size_t i = 0; i < n; i++)
+			/* 根据编号合并两个集合：合并之前要先找根，因此可以复用 voidFindRoot() */
+			int root1 = FindRoot(x1);
+			int root2 = FindRoot(x2);
+
+			if (root1 != root2)
 			{
-				_arr.push_back(arr[i]);
-				_indexMap[arr[i]] = i;
+				_ufs[root1] += _ufs[root2];//谁合并谁都可以，没有特别强制的规定
+				_ufs[root2] = root1;
+				return true;
 			}
+
+			return false;
 		}
+
+		int FindRoot(int x)
+		{
+			/* 根据编号找编号对应元素的根：通过编号存储的父结点，不断跳转到根即可 */
+			int parent = x;
+			while (_ufs[x] >= 0)
+			{
+				parent = _ufs[x];
+				x = parent;
+			}
+
+			return parent;
+		}
+
+		bool InSet(int x, int y)
+		{
+			/* 两编号对应元素是否在同一个集合 */
+			return FindRoot(x) == FindRoot(y);
+		}
+
+		size_t Count()
+		{
+			/* 计算并查集内集合的个数 */
+			size_t count = 0;
+			for (auto it : _ufs)
+			{
+				if (it < 0)
+					++count;
+			}
+
+			return count;
+		}
+
 	private:
-		std::vector<Type> _arr;			//编号找人
-		std::map<Type, int> _indexMap;	//人找编号
+		std::vector<Type> _ufs;
 	};
 }
