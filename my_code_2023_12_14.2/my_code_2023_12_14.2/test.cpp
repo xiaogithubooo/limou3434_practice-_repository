45759194faff6bee29562b97cbe106279ede0bec
limou3434 <898738804@qq.com>
2023-12-17 00:20:41 +0800

    12.17.脚本自动更新仓库limou-c-test-code

---
+++ my_code_2023_12_14.2/my_code_2023_12_14.2/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int number = 0;
+	return 0;
+}
