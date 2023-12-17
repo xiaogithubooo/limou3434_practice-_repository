f6b158bf42b961cb9a4ad6389d5892e627a023ad
limou3434 <898738804@qq.com>
2023-12-17 11:05:33 +0800

    12.17.脚本自动更新仓库limou-c-test-code

--- my_code_2023_12_17/my_code_2023_12_17/main.cpp
+++ my_code_2023_12_17/my_code_2023_12_17/main.cpp
@@ -2,12 +2,68 @@
 #include <vector>
 #include <string>
 using namespace std;
+//
+//class Solution
+//{
+//public:
+//	string minWindow(string s, string t)
+//	{
+//		string ret = s;
+//		int flag = 0;
+//
+//		//1.设置哈希表
+//		int hash1[256] = { 0 };
+//		for (auto it : t)
+//			hash1[it]++;
+//
+//		int hash2[256] = { 0 };
+//		int count = 0;
+//
+//		int len = t.size();
+//		for (int left = 0, right = 0; right < s.size(); right++)
+//		{
+//			//2.进窗口
+//			hash2[s[right]]++;
+//			if (hash2[s[right]] <= hash1[s[right]])
+//			{
+//				count++;
+//			}
+//
+//			//4.更新
+//			if (count == len && ret.size() > s.substr(left, right - left + 1).size())
+//				ret = s.substr(left, right - left + 1);
+//
+//			//3.判断（出窗口）
+//			while (count == len)
+//			{
+//				if (count == len && ret.size() > s.substr(left, right - left + 1).size())
+//					ret = s.substr(left, right - left + 1);
+//
+//				if (hash2[s[left]]-- <= hash1[s[left]])
+//				{
+//					flag = 1;
+//					count--;
+//				}
+//
+//				left++;
+//			}
+//
+//		}
+//
+//		if(flag == 1)
+//			return ret;
+//		return "";
+//	}
+//};
 
 class Solution
 {
 public:
 	string minWindow(string s, string t)
 	{
+		string ret = s;
+		int flag = 0;
+
 		//1.设置哈希表
 		int hash1[256] = { 0 };
 		for (auto it : t)
@@ -16,28 +72,47 @@ public:
 		int hash2[256] = { 0 };
 		int count = 0;
 
+		int len = t.size();
 		for (int left = 0, right = 0; right < s.size(); right++)
 		{
 			//2.进窗口
-			hash2[right]++;
-			if (hash2[right] >= hash1[right])
+			hash2[s[right]]++;
+			if (hash2[s[right]] <= hash1[s[right]])
 			{
 				count++;
 			}
 
-			//3.判断（出窗口）
-			while ()
-			{
+			//4.更新
+			if (count == len && ret.size() > s.substr(left, right - left + 1).size())
+				ret = s.substr(left, right - left + 1);
 
+			//3.判断（出窗口）
+			while (count == len)
+			{
+				if (count == len && ret.size() > s.substr(left, right - left + 1).size())
+					ret = s.substr(left, right - left + 1);
+
+				if (hash2[s[left]]-- <= hash1[s[left]])
+				{
+					flag = 1;
+					count--;
+				}
+
+				left++;
 			}
+
 		}
+
+		if (flag == 1)
+			return ret;
+		return "";
 	}
 };
 
 int main()
 {
 	Solution so;
-	string s = "ADOBECODEBANC", t = "ABC";
-	so.minWindow(s, t);
+	string s = "abc", t = "ac";
+	cout << so.minWindow(s, t);
 	return 0;
 }
