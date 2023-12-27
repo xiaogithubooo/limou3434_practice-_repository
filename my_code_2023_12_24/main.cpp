27117cbe2d42d34a69bc0b061af636a00ad6be7b
limou3434 <898738804@qq.com>
2023-12-27 09:05:39 +0800

    Linux平台下的提交自动更新

--- my_code_2023_12_24/main.cpp
+++ my_code_2023_12_24/main.cpp
@@ -1,43 +1,75 @@
-//多线程抢票
+//多线程抢票（局部锁）
 #include <iostream>
+#include <string>
 #include <pthread.h>
 #include <unistd.h>
+#include <cassert>
 using namespace std;
 
-int ticket = 10000; //票数，模拟临界资源，没有被保护的情况下被并发访问
+int ticket = 10000; //票数，模拟临界资源
+
+class ThreadDatas
+{
+public:
+    ThreadDatas(const string &n, pthread_mutex_t* pm)
+        : _tname(n), _pmtx(pm)
+    {}
+
+    string _tname;
+    pthread_mutex_t* _pmtx;
+};
 
 void* GetTickets(void* args) //模拟临界区代码
 {
-    (void)args;
+    //在临界区内，使用锁来保护临界资源
+    ThreadDatas* td = (ThreadDatas*)args;
+
     while(true)
     {
+        int n = pthread_mutex_lock(td->_pmtx); //上锁
+        assert(n == 0); //检查是否加锁，没有则断死
+
         if(ticket > 0)
         {
-            usleep(1000);
-            printf("%p sells ticket:%d\n", pthread_self(), ticket--);
-            //(1)实际上 -- 操作有三个执行步骤，先从内存中读取数据到 CPU 的上下文寄存器中
-            //(2)CPU 进行自减操作
-            //(3)自减操作做完后，写回进程中
-            //在线程频繁切换中，有可能会触发上下文保护
-            //被切出的线程记录了没有被 -- 操作的上下文
-            //但是下一个线程切入的时候就把 -- 操作做完了
-            //而原先的进程再次切入时，会把上一个线程的结果覆盖，数据就出现了错乱
+            printf("%s sells ticket:%d\n", td->_tname.c_str(), ticket--);
+            pthread_mutex_unlock(td->_pmtx);
         }
+        else
+        {
+            n = pthread_mutex_unlock(td->_pmtx); //解锁
+            assert(n == 0); //检查是否解锁，没有则断死
+
+            break;
+        }
+        usleep(1000); //让线程停一下，保证可以让别的新线程也获得锁
     }
 }
 
+#define NUM 5
 int main()
 {
+    //创建锁
+    pthread_mutex_t mtx;
+    pthread_mutex_init(&mtx, nullptr);
+
     //创建 5 个新线程
-    pthread_t t1, t2, t3;
-    pthread_create(&t1, nullptr, GetTickets, nullptr); //这里的多个执行流同时调用 GetTickets() ，也就是对该函数进行重入
-    pthread_create(&t2, nullptr, GetTickets, nullptr);
-    pthread_create(&t3, nullptr, GetTickets, nullptr);
+    pthread_t ts[NUM] = { 0 };
+    for(int i = 0; i < NUM; i++)
+    {
+        string name = "thread ";
+        name += to_string(i + 1);
+        ThreadDatas* td = new ThreadDatas(name, &mtx);
+        pthread_create(ts + i, nullptr, GetTickets, (void*)td); //这里的多个执行流同时调用 GetTickets() ，也就是对该函数进行重入，最后一个参数就是锁参数
+    }
 
     //等待释放 5 个新线程
-    pthread_join(t1, nullptr);
-    pthread_join(t2, nullptr);
-    pthread_join(t3, nullptr);
+    for(int i = 0; i < NUM; i++)
+    {
+        pthread_join(ts[i], nullptr); //这里的多个执行流同时调用 GetTickets() ，也就是对该函数进行重入
+    }
+    
+    //释放锁
+    pthread_mutex_destroy(&mtx);
 
     return 0;
 }
---
+++ my_code_2023_12_24/test.cpp
@@ -0,0 +1,55 @@
+//多线程抢票（全局锁）
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+#include <cassert>
+using namespace std;
+
+pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER; //定义锁（如果是全局的、静态的，就可以直接使用 PTHREAD_MUTEX_INITIALIZER 宏来初始化，也可以使用函数）
+
+int ticket = 10000; //票数，模拟临界资源，没有被保护的情况下被并发访问
+
+void* GetTickets(void* args) //模拟临界区代码
+{
+    //在临界区内，使用锁来保护临界资源
+    (void)args;
+    while(true)
+    {
+        int n = pthread_mutex_lock(&mtx); //每一个线程都会执行获得锁的动作，但是任何一个时刻只允许一个线程获得，其他线程此时处于阻塞状态，直到锁被解开（并且不一定所有每一个线程都可以平均获得锁）
+        assert(n == 0); //检查是否加锁，没有则断死
+
+        if(ticket > 0)
+        {
+            printf("%p sells ticket:%d\n", pthread_self(), ticket--);
+            pthread_mutex_unlock(&mtx); //解锁，不要放太后，减少加锁的颗粒度，否则打印的效率更低，导致代码整体效率低
+        }
+        else
+        {
+            n = pthread_mutex_unlock(&mtx); //解锁
+            assert(n == 0); //检查是否解锁，没有则断死
+
+            break;
+        }
+        usleep(1000); //让线程停一下，保证可以让别的新线程也获得锁
+    }
+}
+
+int main()
+{
+    //创建 5 个新线程
+    pthread_t t1, t2, t3, t4, t5;
+    pthread_create(&t1, nullptr, GetTickets, nullptr); //这里的多个执行流同时调用 GetTickets() ，也就是对该函数进行重入
+    pthread_create(&t2, nullptr, GetTickets, nullptr);
+    pthread_create(&t3, nullptr, GetTickets, nullptr);
+    pthread_create(&t4, nullptr, GetTickets, nullptr);
+    pthread_create(&t5, nullptr, GetTickets, nullptr);
+
+    //等待释放 5 个新线程
+    pthread_join(t1, nullptr);
+    pthread_join(t2, nullptr);
+    pthread_join(t3, nullptr);
+    pthread_join(t4, nullptr);
+    pthread_join(t5, nullptr);
+    
+    return 0;
+}
