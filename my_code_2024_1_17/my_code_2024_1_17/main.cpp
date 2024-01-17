1fa872fb4bedabc4fad320ce0c95419e6de10c56
limou3434 <898738804@qq.com>
2024-01-17 15:08:45 +0800

    01.17.脚本自动更新仓库limou-c-test-code

--- my_code_2024_1_17/my_code_2024_1_17/main.cpp
+++ my_code_2024_1_17/my_code_2024_1_17/main.cpp
@@ -9,25 +9,23 @@ void Print(int n)
 	for (int i = 0; i < n; ++i)
 	{
 		cout << this_thread::get_id() << " 结果:" << i << '\n';
-	}
+		this_thread::sleep_for(chrono::milliseconds(100)); //休眠 100 微秒再输出
+	}		
+
 }
 
 int main()
 {
-	//vector<thread> threads;
-	//for (int i = 0; i < 3; i++)
-	//{
-	//	threads.push_back(thread(Print, 10));
-	//}
+	vector<thread> threads;
+	for (int i = 0; i < 3; i++)
+	{
+		threads.push_back(thread(Print, 10)); //注意这里只能使用移动构造（线程对象不允许拷贝），因此从这里
+	}
 
-	//for (int i = 0; i < 3; i++)
-	//{
-	//	threads[i].join();
-	//}
-
-	char ch = 'A';
-	int number = int(ch); //等价于 (int)ch
-	cout << ch << " = " << number << endl;
+	for (int i = 0; i < 3; i++)
+	{
+		threads[i].join();
+	}
 
 	return 0;
 }
