759631fff657bee3c46b11cc52e0155cfbe7c495
limou3434 <898738804@qq.com>
2024-01-18 15:47:39 +0800

    1.18.归类仓库文件，划分每一年的代码练习

--- limou-c-test-code/main.c
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include "function.h"
-int main()
-{
-	number_table A;
-	function_table_n_IO(&A, 5, 1);
-	function_table_n_IO(&A, 5, 0);
-	function_table_n_IO(&A, 5, 2);
-}
--- limou-c-test-code/my_code_2023_2_2_dev.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-	char * a = "hello word";
-	a[1] = 'E';
-	printf("hello word");
-	printf("%s\n", a);
-	return 0;
-}
