3927b489b1e4481334c36e55dcb2ec4adea3ddfe
limou3434 <898738804@qq.com>
2024-01-21 10:27:41 +0800

    1.21.关于cin和string内输入使用getline的区别和研究

--- 2024归档/my_code_2023_1_20.3/my_code_2023_1_20.3/main.cpp
+++ 2024归档/my_code_2023_1_20.3/my_code_2023_1_20.3/main.cpp
@@ -1,23 +1,42 @@
+//string 的成员对象 cin 的输入用法
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main()
 {
-
-	//第一种输入（忽略空格、忽略换行符）
 	string str;
-	cin >> str;
-	cout << "str:" << str << endl;
-	cin.get(); //只读取一个字符
+	char ch = 0;
 
-	//第二种输入（读取输入，直到遇到换行符，并且抛弃换行符）
+
+	//第一种输入（跳过输入流中的所有空格字符，直到遇到非空格字符为止，开始读取）
+	cout << "第一种输入:" << '\n';
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> str;
+		cout << "str:" << str << '\n';
+		ch = cin.get(); //只读取一个字符，去除换行符（这里可以不用，是因为就算被 str 读取了也会被跳过）
+	}
+	//ch = cin.get(); //如果上面循环中的 ch = cin.get(); 不用那么这里就应该加上 ch = cin.get(); 否则影响后面输入
+
+
+	//第二种输入（跳过输入流中的所有 ',' 字符，直到遇到非 ',' 字符为止，开始读取）
+	cout << "第二种输入:" << '\n';
+	for (int i = 0; i < 5; i++)
+	{
+		getline(cin, str, ',');
+		cout << "str:" << str << '\n';
+		if (i == 5)
+			break;
+		ch = cin.get(); //只读取一个字符，去除换行符
+	}
+
+
+	//第三种输入（一直读取输入，直到遇到换行符，并且直接抛弃换行符，不放回输入缓冲）
+	cout << "第三种输入:" << '\n';
 	getline(cin, str);
-	const char* s = str.c_str();
-	cout << "str:" << str << endl;
-	cin.get(); //只读取一个字符
+	cout << "str:" << str << '\n';
 
-	//第三种输入（）
 	return 0;
 }
 
