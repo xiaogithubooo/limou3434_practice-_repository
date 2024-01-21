bec5d9be95ffe3cb39a135d20ed08f9db75eaa9f
limou3434 <898738804@qq.com>
2024-01-21 16:02:40 +0800

    01.21.脚本自动更新仓库limou-c-test-code

---
+++ 2024归档/my_code_2024_1_21/my_code_2024_1_21/mian.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cout << "\"Hello, I'am limou3434.\"\n" << endl; //标准字符串
+	cout << R"**(\"Hello, I')" limou3434.\"\n)**" << endl; //原始字符串
+	return 0;
+}
