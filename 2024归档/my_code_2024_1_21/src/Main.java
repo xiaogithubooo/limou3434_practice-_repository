5aeaa4362f9cd89eeb3491f7aa17101dd38dc799
limou3434 <898738804@qq.com>
2024-01-21 14:44:49 +0800

    1.21.关于cin和string内输入使用getline的区别和研究

---
+++ 2024归档/my_code_2024_1_21/src/Main.java
@@ -0,0 +1,32 @@
+//自定义构造方法
+class Data {
+    public int _year;
+    public int _month;
+    public int _day;
+
+    Data() {}
+
+    public void SetData(int year, int month, int day) {
+        _year = year;
+        _month = month;
+        _day = day;
+        this.Print(); //设定好值后就打印
+    }
+
+    public void Print() {
+        System.out.println(_year + " " + _month + " " + _day);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Data d = new Data();
+        d.Print();
+    }
+}
+
+/* 输出结果
+带参数的构造方法
+不带参数的构造方法
+2000 1 1
+*/
