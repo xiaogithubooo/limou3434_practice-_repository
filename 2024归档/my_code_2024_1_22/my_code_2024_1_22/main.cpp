a1cff94bcc756654ffc7ae80c198a004adbc4f39
limou3434 <898738804@qq.com>
2024-01-22 16:40:42 +0800

    1.22.了解和学习C++序列化的库

--- 2024归档/my_code_2024_1_22/my_code_2024_1_22/main.cpp
+++ 2024归档/my_code_2024_1_22/my_code_2024_1_22/main.cpp
@@ -1,90 +1,90 @@
-#include <iostream>
-#include <map>
-using namespace std;
-
-// 懒汉模式:第一次用的时候再创建（现吃现做）
-// todo:线程安全问题
-// new的懒汉对象一般不需要释放，进程正常结束会释放资源
-// 如果需要做一些动作，比如持久化，那么可以利用gc类static对象搞定
-template <class Type = map<string, string>>
-class B
-{
-public:
-	static B* GetInstance()
-	{
-		if (_inst == nullptr)
-		{
-			_inst = new B;
-		}
-
-		return _inst;
-	}
-
-	void Add(const string& key, const string& value)
-	{
-		_dict[key] = value;
-	}
-
-	void Print()
-	{
-		for (auto& kv : _dict)
-		{
-			cout << kv.first << ":" << kv.second << endl;
-		}
-		cout << endl;
-	}
-
-	static void DelInstance()
-	{
-		if (_inst)
-		{
-			delete _inst;
-			_inst = nullptr;
-		}
-	}
-
-	class gc
-	{
-	public:
-		~gc()
-		{
-			DelInstance();
-		}
-	};
-
-private:
-	B()
-	{}
-
-	~B()
-	{
-		// 持久化：要求把数据写到文件
-		cout << "数据写到文件" << endl;
-	}
-
-	B(const B& aa) = delete;
-	B& operator=(const B& aa) = delete;
-
-private:
-	Type _dict;
-	int _n = 0;
-	static B* _inst;
-	static gc _gc;
-};
-
-template <class Type>
-B<Type>* B<Type>::_inst = nullptr;
-
-template <class Type>
-typename B<Type>::gc B<Type>::_gc;
-
-template class B<map<string, string>>;
-int main()
-{
-	B<>::GetInstance()->Add("1", "limou");
-	B<>::GetInstance()->Add("2", "dimou");
-	B<>::GetInstance()->Add("3", "eimou");
-	B<>::GetInstance()->Add("4", "iimou");
-	B<>::GetInstance()->Print();
-	return 0;
-}
+//#include <iostream>
+//#include <map>
+//using namespace std;
+//
+//// 懒汉模式:第一次用的时候再创建（现吃现做）
+//// todo:线程安全问题
+//// new的懒汉对象一般不需要释放，进程正常结束会释放资源
+//// 如果需要做一些动作，比如持久化，那么可以利用gc类static对象搞定
+//template <class Type = map<string, string>>
+//class B
+//{
+//public:
+//	static B* GetInstance()
+//	{
+//		if (_inst == nullptr)
+//		{
+//			_inst = new B;
+//		}
+//
+//		return _inst;
+//	}
+//
+//	void Add(const string& key, const string& value)
+//	{
+//		_dict[key] = value;
+//	}
+//
+//	void Print()
+//	{
+//		for (auto& kv : _dict)
+//		{
+//			cout << kv.first << ":" << kv.second << endl;
+//		}
+//		cout << endl;
+//	}
+//
+//	static void DelInstance()
+//	{
+//		if (_inst)
+//		{
+//			delete _inst;
+//			_inst = nullptr;
+//		}
+//	}
+//
+//	class gc
+//	{
+//	public:
+//		~gc()
+//		{
+//			DelInstance();
+//		}
+//	};
+//
+//private:
+//	B()
+//	{}
+//
+//	~B()
+//	{
+//		// 持久化：要求把数据写到文件
+//		cout << "数据写到文件" << endl;
+//	}
+//
+//	B(const B& aa) = delete;
+//	B& operator=(const B& aa) = delete;
+//
+//private:
+//	Type _dict;
+//	int _n = 0;
+//	static B* _inst;
+//	static gc _gc;
+//};
+//
+//template <class Type>
+//B<Type>* B<Type>::_inst = nullptr;
+//
+//template <class Type>
+//typename B<Type>::gc B<Type>::_gc;
+//
+//template class B<map<string, string>>;
+//int main()
+//{
+//	B<>::GetInstance()->Add("1", "limou");
+//	B<>::GetInstance()->Add("2", "dimou");
+//	B<>::GetInstance()->Add("3", "eimou");
+//	B<>::GetInstance()->Add("4", "iimou");
+//	B<>::GetInstance()->Print();
+//	return 0;
+//}
--- 2024归档/my_code_2024_1_22/my_code_2024_1_22/test.cpp
+++ 2024归档/my_code_2024_1_22/my_code_2024_1_22/test.cpp
@@ -1,83 +1,19 @@
-//#include <iostream>
-//#include <map>
-//using namespace std;
-//
-//class B
-//{
-//public:
-//	static B* GetInstance()
-//	{
-//		if (_inst == nullptr)
-//		{
-//			_inst = new B;
-//		}
-//
-//		return _inst;
-//	}
-//
-//	void Add(const string& key, const string& value)
-//	{
-//		_dict[key] = value;
-//	}
-//
-//	void Print()
-//	{
-//		for (auto& kv : _dict)
-//		{
-//			cout << kv.first << ":" << kv.second << endl;
-//		}
-//		cout << endl;
-//	}
-//
-//	static void DelInstance()
-//	{
-//		if (_inst)
-//		{
-//			delete _inst;
-//			_inst = nullptr;
-//		}
-//	}
-//
-//private:
-//	B()
-//	{}
-//
-//	~B()
-//	{
-//		// 持久化：要求把数据写到文件
-//		cout << "数据写到文件" << endl;
-//	}
-//
-//	B(const B& aa) = delete;
-//	B& operator=(const B& aa) = delete;
-//
-//	map<string, string> _dict;
-//	int _n = 0;
-//
-//	static B* _inst;
-//
-//	class gc
-//	{
-//	public:
-//		~gc()
-//		{
-//			DelInstance();
-//		}
-//	};
-//
-//	static gc _gc;
-//};
-//
-//B* B::_inst = nullptr;
-//B::gc B::_gc;
-//
-//int main()
-//{
-//	B::GetInstance()->Add("1", "limou");
-//	B::GetInstance()->Add("2", "dimou");
-//	B::GetInstance()->Add("3", "eimou");
-//	B::GetInstance()->Add("4", "iimou");
-//	B::GetInstance()->Print();
-//
-//	return 0;
-//}
+//字符串拼接
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	stringstream ss;
+	//将多个字符串放入 ss 中
+	ss << "first" << " " << "string,";
+	ss << " second string";
+	cout << "strResult is: " << ss.str() << endl;
+
+	//清空 ss
+	ss.str("");
+	ss << "third string";
+	cout << "After clear, strResult is: " << ss.str() << endl;
+	return 0;
+}
