7f358066f383a7d16871b2fc0124a113104e53ac
limou3434 <898738804@qq.com>
2024-01-28 17:15:45 +0800

    1.28.Linux下验证代码，改动父子进程的匿名管道的读写关系

--- 2024归档/my_code_2024_1_27/main.cpp
+++ 2024归档/my_code_2024_1_27/main.cpp
@@ -1,4 +1,4 @@
-//使用匿名管道（父进程写，子进程读）
+//使用匿名管道（父进程读，子进程写）
 #include <iostream>
 #include <string>
 
@@ -26,70 +26,74 @@ int main()
     assert(id != -1);
     
     //3.创建信道
-    //3.1.子进程执行部分（子进程为读端）
+    //3.1.子进程执行部分（子进程为写端）
     if (id == 0)
     {
-        close(pipefd[1]); //关闭子进程的写端
-        char buffer[1024] = { 0 }; //自定义读缓冲区
+        close(pipefd[0]); //关闭子进程读端
+        char send_buffer[1024] = { 0 }; //自定义写缓冲区
 
         while (true)
         {
             //3.1.1.正常写入管道文件
-            ssize_t s = read(pipefd[0], buffer, sizeof(buffer) - 1); //读取管道数据到自定义的缓冲区中
-            if (s > 0)
-            {
-                sleep(3); //这里加上这一句的目的是想查看，父进程写完字符串 "exit" 后，比子进程读取先关闭了写端，则子进程是否会进行正常读取（也可以测试多次写端多次输入后，读端是否全部读取）
-                cout << "读取到的字符个数:" << s << endl;
-                buffer[s] = 0; //添加 '\0' 因为系统调用不靠该字符结尾
-                cout << "[" << getpid() << "]:child get a message" << ", Father sad:[" << buffer << "]" << endl;
-                //一旦管道文件不存在数据，子进程读完后就会陷入阻塞状态...
+            string message;
+            cin >> message; //用户输入消息
+            
+            snprintf(send_buffer, sizeof(send_buffer), "%s", message.c_str()); //写到自定义的缓冲区中
+            write(pipefd[1], send_buffer, strlen(send_buffer)); //写到管道文件中
 
-                //3.1.2.关闭管道文件的读端
-                if (strcmp(buffer, "read_exit") == 0)
-                {
-                    cout << "子进程关闭读端" << endl;
-                    close(pipefd[0]);
-                    break;
-                }
-            }
-            else if (s == 0)
+            //3.1.2.关闭管道文件的写端
+            if (message == "write_exit")
             {
-                cout << "父进程写端被关闭，read() 返回值为:" << s << endl;
+                cout << "子进程关闭写端" << endl;
+                close(pipefd[1]); //检测父进程提前关闭写端的现象
                 break;
             }
         }
+        
         exit(10);
     }
 
-    //3.2.父进程执行部分（父进程为写段）
-    close(pipefd[0]); //关闭父进程读端
-    char send_buffer[1024] = { 0 }; //自定义写缓冲区
+    //3.2.父进程执行部分（父进程为读段）
+    close(pipefd[1]); //关闭父进程的写端
+    char buffer[1024] = { 0 }; //自定义读缓冲区
 
     while (true)
     {
-        //3.2.1.正常写入管道文件
-        string message;
-        cin >> message; //用户输入消息
-        
-        snprintf(send_buffer, sizeof(send_buffer), "%s", message.c_str()); //写到自定义的缓冲区中
-        write(pipefd[1], send_buffer, strlen(send_buffer)); //写到管道文件中
-
-        //3.2.2.关闭管道文件的写端
-        if (message == "write_exit")
+        //3.2.1.正常读取管道文件
+        ssize_t s = read(pipefd[0], buffer, sizeof(buffer) - 1); //读取管道数据到自定义的缓冲区中
+        if (s > 0)
         {
-            cout << "父进程关闭写端" << endl;
-            close(pipefd[1]); //检测父进程提前关闭写端的现象
+            sleep(3); //这个睡眠秒数在检验一些情况的时候可以稍微调整一下
+            cout << "读取到的字符个数:" << s << endl;
+            buffer[s] = 0; //添加 '\0' 因为系统调用不靠该字符结尾
+            cout << "[" << getpid() << "]:Father get a message" << ", child sad:[" << buffer << "]" << endl;
+
+            //3.1.2.关闭管道文件的读端
+            if (strcmp(buffer, "read_exit") == 0)
+            {
+                cout << "父进程关闭读端" << endl;
+                close(pipefd[0]);
+                break;
+            }
+        }
+        else if(s == 0)
+        {
+            cout << "子进程将写端关闭了，read() 直接返回:" << s << endl;
             break;
+        }
+        else
+        {
+            //todo...
         }
     }
 
     //4.等待和检测子进程信息
     int status = 0; //获取子进程信号
-    pid_t ret = waitpid(id, nullptr, 0); //等待子进程
+    pid_t ret = waitpid(id, &status, 0); //等待子进程
     assert(ret > 0), (void)ret; //检查和消除警告
 
-    printf("I am father process, wait success, pid: %d, rpid: %d, exit code: %d, exit sig: %d\n"
-        , getpid(), getppid(), (status >> 8) & 0xFF, status & 0x7F);
+    printf("I am father process, wait success, my pid: %d, sub pid: %d, exit code: %d, exit sig: %d\n"
+        , getpid(), id, (status >> 8) & 0xFF, status & 0x7F);
     
     return 0;
 }
