76577b5312bbb3d93223f39d5ac9f65260d051d7
limou3434 <898738804@qq.com>
2024-02-06 22:19:02 +0800

    2.6.关于进程信号的复习和研究，还有设定闹钟的学习

--- 2024归档/my_code_2024_2_5/sig/main.cpp
+++ 2024归档/my_code_2024_2_5/sig/main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <unistd.h>
+#include <signal.h>
+
+unsigned int Seconds = 100;
+int n = 0;
+
+void alarmHandler(int signum)
+{
+    n = alarm(0);
+    std::cout << n << std::endl;
+    alarm(n);
+}
+
 int main()
 {
+    signal(SIGALRM, alarmHandler);
+    alarm(Seconds);
+    std::cout << "Alarm set for " << Seconds << " seconds" << std::endl;
+    
+    while (true)
+    {
+        sleep(1);
+    }
+
     return 0;
 }
