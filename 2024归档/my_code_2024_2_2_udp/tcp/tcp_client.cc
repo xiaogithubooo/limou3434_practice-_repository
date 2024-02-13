ebc52b3d67d59f73506461688420021de22f5eb1
limou3434 <898738804@qq.com>
2024-02-13 23:45:03 +0800

    2.13.初步使用TCP接口来编写客户端代码，并且通过测试

--- 2024归档/my_code_2024_2_2_udp/tcp/tcp_client.cc
+++ 2024归档/my_code_2024_2_2_udp/tcp/tcp_client.cc
@@ -2,6 +2,10 @@
 #include <iostream>
 #include <string>
 
+#include <cstdio>
+#include <cstring>
+
+#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
@@ -14,14 +18,14 @@ static void Usage(std::string proc)
 
 int main(int argc, char* argv[])
 {
-    if (argc != 2)
+    if (argc != 3)
     {
         Usage(argv[0]);
         exit(-1);
     }
 
-    std::string ip = argv[1];
-    uint16_t port = atoi(argv[2]);
+    std::string serverIp = argv[1];
+    uint16_t serverPort = atoi(argv[2]);
 
     int sock = socket(
         AF_INET, //协议家族
@@ -29,9 +33,40 @@ int main(int argc, char* argv[])
         0 //协议类别
     );
 
-    //无需 bind
+    //无需 bind，让 OS 自己选择，在 connect() 链接服务器中会自动绑定
+    struct sockaddr_in server;
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(serverPort);
+    server.sin_addr.s_addr = inet_addr(serverIp.c_str());
+    if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0) //链接客户端
+    {
+        std::cerr << "connect error" << std::endl;
+        exit(-1);
+    }
 
-    
+    std::cout << "connect success" << std::endl;
+
+    while (true)
+    {
+        std::string line;
+        std::cout << "insert>:";
+        std::getline(std::cin, line);
+        send(sock, line.c_str(), line.size(), 0); //类似 write()，send() 专门给 TCP 来用，send_to() 专门给 UDP 来用，最后一个参数一般是 0
+
+        char buffer[1024] = { 0 };
+        ssize_t s = recv(sock, buffer, sizeof(buffer) - 1, 0); //类似 read()，读取服务器的返回值
+        if (s > 0)
+        {
+            std::cout << "server 回显" << buffer << std::endl;
+        }
+        else //对端关闭链接或者读取失败
+        {
+            break;
+        }
+    }
+
+    close(sock);
 
     return 0;
 }
