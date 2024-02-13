eb0806fbeb1395f63c69233fe550962399acf641
limou3434 <898738804@qq.com>
2024-02-13 21:37:15 +0800

    Linux平台下的提交自动更新

--- 2024归档/my_code_2024_2_2_udp/tcp/tcp_client.cc
+++ 2024归档/my_code_2024_2_2_udp/tcp/tcp_client.cc
@@ -1,7 +1,37 @@
 //tcp_client.cc
 #include <iostream>
+#include <string>
 
-int main()
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+static void Usage(std::string proc)
 {
+    std::cout << "\nUsage:" << proc << "serverIp serverProt\n" << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        Usage(argv[0]);
+        exit(-1);
+    }
+
+    std::string ip = argv[1];
+    uint16_t port = atoi(argv[2]);
+
+    int sock = socket(
+        AF_INET, //协议家族
+        SOCK_STREAM, //通信类别
+        0 //协议类别
+    );
+
+    //无需 bind
+
+    
+
     return 0;
 }
--- 2024归档/my_code_2024_2_2_udp/tcp/tcp_server.cc
+++ 2024归档/my_code_2024_2_2_udp/tcp/tcp_server.cc
@@ -12,6 +12,7 @@ int main(int argc, char* argv[])
     if (argc != 2)
     {
         Usage(argv[0]);
+        exit(-1);
     }
 
     uint16_t port = atoi(argv[1]);
