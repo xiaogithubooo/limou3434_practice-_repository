bca07974933cd81fe4e4cf297b35532a8cd2b08a
limou3434 <898738804@qq.com>
2024-02-26 20:16:03 +0800

    02.26. Java学习

--- 2024归档/my_code_2024_1_21/src/Main.java
+++ 2024归档/my_code_2024_1_21/src/Main.java
@@ -1,33 +1,12 @@
-//自定义构造方法
-class Data {
-    public int _year;
-    public int _month;
-    public int _day;
-
-    Data()
-    {}
-
-    public void SetData(int year, int month, int day) {
-        _year = year;
-        _month = month;
-        _day = day;
-        this.Print(); //设定好值后就打印
-    }
-
-    public void Print() {
-        System.out.println(_year + " " + _month + " " + _day);
-    }
-}
-
+import Person
+//主类的主函数
 public class Main {
     public static void main(String[] args) {
-        Data d = new Data();
-        d.Print();
+        Person per = new Person(); //使用 new 语法创建（实例化对象）
+        per.name = "limou3434";
+        per.age = 18;
+
+        System.out.println("姓名:" + per.GetName());
+        System.out.println("年龄:" + per.GetAge());
     }
 }
-
-/* 输出结果
-带参数的构造方法
-不带参数的构造方法
-2000 1 1
-*/
