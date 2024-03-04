92400b54ae18506fd8ddec4adec2bd08b4146662
limou3434 <898738804@qq.com>
2024-03-04 21:36:11 +0800

    03.04.脚本自动更新仓库limou-c-test-code

--- 2024归档/my_code_2024_1_21/src/Main.java
+++ 2024归档/my_code_2024_1_21/src/Main.java
@@ -1,22 +1,15 @@
-import java.io.Console;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        // 获取控制台对象
-        Console console = System.console();
-
-        if (console != null) {
-            // 读取用户输入的用户名
-            String username = console.readLine("请输入用户名：");
-
-            // 读取用户输入的密码（不显示在控制台上）
-            char[] password = console.readPassword("请输入密码：");
-
-            // 对用户输入的用户名和密码进行处理
-            System.out.println("用户名：" + username);
-            System.out.println("密码：" + new String(password));
-        } else {
-            System.out.println("未能获取控制台对象");
+        int[][] arr_1 = new int[10][];
+        for (int i = arr_1.length - 1; i >= 0; i--) {
+            arr_1[i] = new int[i + 1];
+            for (int j = i; j >= 0; j--) {
+                arr_1[i][j] = 10;
+            }
         }
+
+        System.out.println(Arrays.deepToString(arr_1));
     }
 }
