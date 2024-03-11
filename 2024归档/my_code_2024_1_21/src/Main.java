427d9e2b479346adce5abc06c4256db86a2d57c9
limou3434 <898738804@qq.com>
2024-03-11 22:45:02 +0800

    03.11.脚本自动更新仓库limou-c-test-code

--- 2024归档/my_code_2024_1_21/src/Main.java
+++ 2024归档/my_code_2024_1_21/src/Main.java
@@ -1,15 +1,20 @@
-import java.util.Arrays;
+class MyClass {
+    // 实例内部类/非静态内部类
+    public int data = 1;
+    class Sub {
+        public int data = 2;
+
+        public void Func() {
+            System.out.println(this.data); // 获取内部对象
+            System.out.println(MyClass.this.data); //获取外部对象
+        }
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        int[][] arr_1 = new int[10][];
-        for (int i = arr_1.length - 1; i >= 0; i--) {
-            arr_1[i] = new int[i + 1];
-            for (int j = i; j >= 0; j--) {
-                arr_1[i][j] = 10;
-            }
-        }
 
-        System.out.println(Arrays.deepToString(arr_1));
+        MyClass mc = new MyClass();
+        mc.new Sub().Func();
     }
 }
