8d7e65d6a16d7f6104bea2849562ec56f41c18e2
limou3434 <898738804@qq.com>
2024-03-12 10:45:55 +0800

    03.12.脚本自动更新仓库limou-c-test-code

--- 2024归档/my_code_2024_1_21/src/Main.java
+++ 2024归档/my_code_2024_1_21/src/Main.java
@@ -1,20 +1,38 @@
-class MyClass {
+class Outer {
     // 实例内部类/非静态内部类
-    public int data = 1;
-    class Sub {
-        public int data = 2;
+    public int data_1 = 1;
+    public int data_2 = 2;
+    public static int data_3 = 3;
 
-        public void Func() {
-            System.out.println(this.data); // 获取内部对象
-            System.out.println(MyClass.this.data); //获取外部对象
+    public void Test_1() {
+        System.out.println("Outer::test_1()");
+    }
+
+    static class Inner {
+        public int data_3 = 4;
+        public int data_4 = 5;
+        public static int data_5 = 6;
+
+        public void Test_2() {
+            System.out.println("Inner::test_2()");
+            // 静态内部类无法访问外部类成员，因为其本身不需要使用外部类来创建实例对象
+            // System.out.println(data_1);
+            // System.out.println(data_2);
+
+            // 但是依旧是有方法的，可以创建一个外部类成员
+            Outer o = new Outer();
+            System.out.println(o.data_1);
+
+            System.out.println(data_3);
+            System.out.println(data_4);
+            System.out.println(data_5);
         }
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-
-        MyClass mc = new MyClass();
-        mc.new Sub().Func();
+        Outer.Inner in = new Outer.Inner(); // 无需实例化外部类对象就可以使用
+        in.Test_2();
     }
 }
