ed43d9979fc3fc66965f5f5d6100d638c658f070
limou3434 <898738804@qq.com>
2024-03-14 21:40:49 +0800

    Linux平台下的提交自动更新

---
+++ 2024归档/my_code_2024_3_14.2/blocking_queue.cpp
@@ -0,0 +1,48 @@
+//blocking_queue.cpp
+#include "blocking_queue.hpp"
+
+//生产者方法：生产任务
+void* Producer(void* args)
+{
+    //强制参数得到队列
+    BlockingQueue<int>* bq = static_cast<(BlockingQueue<int>*)>args;
+    int a = 1; //起始数据
+    while(true)
+    {
+        bq->Push(a); //生产者生产数据
+        std::cout << "Produce data: " << a++ << '\n';
+    }
+    return nullptr;
+}
+
+//消费者方法
+void* Consumer(void* args)
+{
+    //强制参数得到队列
+    BlockingQueue<int>* bq = static_cast<(BlockingQueue<int>*)>args;
+    int a = 0; //临时变量
+    while(true)
+    {
+        bq->Pop(&a); //消费者消费数据
+        std::cout << "Consumption data:" << a << '\n';
+        sleep(5);
+    }
+    return nullptr;
+}
+
+int main()
+{
+    //创建阻塞队列（也就是市场）
+    BlockingQueue<int>* bq = new BlockingQueue<int>(); //不直接使用 STL 队列的原因是原生队列不是线程安全的
+
+    //创建生产者和消费者进程
+    pthread_t p; //单个生产者线程
+    pthread_t c; //单个消费者线程
+    pthread_create(&p, nullptr, Consumer, bq);
+    pthread_create(&c, nullptr, Producer, bq);
+
+    //等待释放两种线程
+    pthread_join(p, nullptr);
+    pthread_join(c, nullptr);
+    return 0;
+}
---
+++ 2024归档/my_code_2024_3_14.2/blocking_queue.hpp
@@ -0,0 +1,92 @@
+//blocking_queue.hpp
+#pragma once
+#include <iostream>
+#include <queue>
+#include <unistd.h>
+#include <pthread.h>
+
+const int gDefaultCap = 5;
+
+template<typename T>
+class BlockingQueue
+{
+    private:bool IsQueueEmpty()
+    {
+        return _bq.size() == 0;
+    }
+    private:bool IsQueueFull()
+    {
+        return _bq.size() == _capacity;
+    }
+
+    //初始化锁和条件变量
+    public:BlockingQueue(int capacity = gDefaultCap) //默认为上限为 5
+        : _capacity(capacity)
+    {
+        pthread_mutex_init(&_mutex, nullptr);
+        pthread_cond_init(&_p_cond, nullptr);
+        pthread_cond_init(&_c_cond, nullptr);
+    }
+
+    //生产者产数据
+    public:void Push(const T& in)
+    {
+        //加锁，防止消费者非法消费
+        pthread_mutex_lock(&_mutex);
+
+        //检测临界资源是否满足访问条件（队列是否为满）
+        if (IsQueueFull())
+            pthread_cond_wait(&_full, &_mutex);
+        //第二个参数是锁的原因是：
+        //生产者是带着锁去阻塞的，那消费者就无法拿到锁，
+        //最终生产者的挂起是没有效果的，本来我们挂起就是为了让消费者去消费资源...
+        //因此第二个参数可以传递锁，在函数内的锁会被自动解开
+
+        //如果线程在这行之前曾被挂起，则会从这里继续运行/唤醒，唤醒的时候 pthread_cond_wait() 会自动为线程获取锁
+         
+        //访问临界资源，生产数据
+        _bq.push(in);
+
+        //解锁
+        pthread_mutex_unlock(&_mutex);
+
+        //唤醒消费者
+        pthread_cond_signal(&_empty);
+    }
+
+    //消费者取数据
+    public:void Pop(T* out)
+    {
+        //加锁
+        pthread_mutex_lock(&_mutex);
+
+        //检测临界资源是否满足访问条件（队列是否为空）
+        if(IsQueueEmpty()) pthread_cond_wait(&_empty, &_mutex);
+
+        //访问临界资源，取得数据
+        *out = _bq.front();
+        _bq.pop();
+
+        //解锁
+        pthread_mutex_unlock(&_mutex);
+
+        //唤醒生产者
+        pthread_cond_signal(&_full);
+
+    }
+
+    public:~BlockingQueue()
+    {
+        pthread_mutex_destroy(&_mutex);
+        pthread_cond_destroy(&_empty);
+        pthread_cond_destroy(&_full);
+    }
+
+private:
+    std::queue<T> _bq; //阻塞队列，存储数据
+    int _capacity; //容量上限
+    pthread_mutex_t _mutex; //通过互斥锁保护队列安全（如果不用条件变量，那么线程每一次都会去申请锁来检查队列的情况）
+    pthread_cond_t _p_cond; //通过条件变量阻塞队列（该条件变量表示 _bq 为满，可以让生产者停止生产）
+    pthread_cond_t _c_cond; //通过条件变量阻塞队列（该条件变量表示 _bq 为空，可以让消费者停止消费）
+    /* 而谁来唤醒生产者和消费者呢？实际上双方互相提醒是最合适的 */
+};
