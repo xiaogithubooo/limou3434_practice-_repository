5b56ec5e1537c7aa4b58b56cf7b04a48df11e545
limou3434 <898738804@qq.com>
2024-03-15 15:27:36 +0800

    3.15.关于单角色的生产者-消费者模型

--- 2024归档/my_code_2024_3_14.2/blocking_queue.cpp
+++ 2024归档/my_code_2024_3_14.2/blocking_queue.cpp
@@ -1,34 +1,45 @@
 //blocking_queue.cpp
+#include <stdlib.h>
+#include <unistd.h>
 #include "blocking_queue.hpp"
+#include "task.hpp"
 
 int productionSpeed = 1; //生产速率
 int consumptionSpeed = 5; //消费/速率
+
+std::string opers = "+-*/%";
 
 //生产者方法：生产任务
 void* Producer(void* args)
 {
     //强制参数得到队列
-    BlockingQueue<int>* bq = static_cast<BlockingQueue<int>*>(args);
-    int a = 1; //起始数据
-    while(true)
+    BlockingQueue<Task>* bq = static_cast<BlockingQueue<Task>*>(args);
+    while (true)
     {
-        bq->Push(a); //生产者生产数据
-        std::cout << "Produce data: " << a++ << '\n';
+        //生成两个随机数
+        int data_1 = rand() % 10;
+        usleep(rand() % 100); //避免两个随机值一样
+        int data_2 = rand() % 10;
+        char oper = opers[rand() % opers.size()];
+
+        Task t(data_1, data_2, oper);
+        bq->Push(t); //生产者生产数据
+        std::cout << "Produce data: " << t.Show() << std::endl;
         sleep(productionSpeed);
     }
     return nullptr;
 }
 
-//消费者方法
+//消费者方法：消费任务
 void* Consumer(void* args)
 {
     //强制参数得到队列
-    BlockingQueue<int>* bq = static_cast<BlockingQueue<int>*>(args);
-    int a = 0; //临时变量
+    BlockingQueue<Task>* bq = static_cast<BlockingQueue<Task>*>(args);
     while(true)
-    {
-        bq->Pop(&a); //消费者消费数据
-        std::cout << "Consumption data:" << a << '\n';
+    { 
+        Task t; //临时变量
+        bq->Pop(&t); //消费者消费数据
+        std::cout << "Consumption data: " << t.Show() << "=" << t() << std::endl;
         sleep(consumptionSpeed);
     }
     return nullptr;
@@ -36,8 +47,11 @@ void* Consumer(void* args)
 
 int main()
 {
+    //初始随机种子
+    srand((uint16_t)time(nullptr) ^ getpid() ^ pthread_self());
+
     //创建阻塞队列（也就是市场）
-    BlockingQueue<int>* bq = new BlockingQueue<int>(); //不直接使用 STL 队列的原因是原生队列不是线程安全的
+    BlockingQueue<Task>* bq = new BlockingQueue<Task>(); //不直接使用 STL 队列的原因是原生队列不是线程安全的
 
     //创建生产者和消费者进程
     pthread_t p; //单个生产者线程
--- 2024归档/my_code_2024_3_14.2/lock_guard.hpp
+++ 2024归档/my_code_2024_3_14.2/lock_guard.hpp
@@ -6,20 +6,19 @@
 //封装锁
 class Mutex
 {
-public:
-   Mutex(pthread_mutex_t* mtx)
+    public:Mutex(pthread_mutex_t* mtx)
        : _pmtx(mtx)
-   {}
-   void Lock()
-   {
-       //std::cout << "智能加锁" << '\n';
-       pthread_mutex_lock(_pmtx);
-   }
-   void UnLock()
-   {
-       //std::cout << "智能解锁" << '\n';
-       pthread_mutex_unlock(_pmtx);
-   }
+    {}
+    public:void Lock()
+    {
+        //std::cout << "智能加锁" << '\n';
+        pthread_mutex_lock(_pmtx);
+    }
+    public:void UnLock()
+    {
+        //std::cout << "智能解锁" << '\n';
+        pthread_mutex_unlock(_pmtx);
+    }
 
 private:
    pthread_mutex_t* _pmtx;
@@ -28,17 +27,16 @@ private:
 //智能锁（有点类似智能指针）
 class LockGuard
 {
-public:
-   LockGuard(pthread_mutex_t* mtx)
-       : _mtx(mtx)
-   {
-       _mtx.Lock();
-   }
-   ~LockGuard()
-   {
-       _mtx.UnLock();
-   }
+    public:LockGuard(pthread_mutex_t* mtx)
+        : _mtx(mtx)
+    {
+        _mtx.Lock();
+    }
+    public:~LockGuard()
+    {
+        _mtx.UnLock();
+    }
 
 private:
-   Mutex _mtx;
+    Mutex _mtx;
 };
