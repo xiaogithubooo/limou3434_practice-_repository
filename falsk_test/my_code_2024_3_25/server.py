01ae58a1e228bfe670f39aeb24b5040881b20a73
limou3434 <898738804@qq.com>
2024-03-26 15:48:24 +0800

    使用 Jinja2 和 Falsk 框架

--- falsk_test/my_code_2024_3_25/server.py
+++ falsk_test/my_code_2024_3_25/server.py
@@ -1,16 +1,14 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html') # 渲染 templates 文件夹下的 index.html 页面
+    return render_template('index.html')
 
-@app.route('/get_hello', methods=['POST']) # 用户点击按钮后，通过 fetch() 跳转到下面的视图函数
-def get_hello():
-    data = request.get_json()  # 获取客户端发送的 JSON 数据
-    message = data['name'] + '3434' # 加工用户发送过来的数据
-    return jsonify({'message': message}) # 以 JSON 的显示转回客户端
+@app.route('/<int:args>')
+def detail(args):
+    return render_template('index.html', args_value=args)
 
 if __name__ == '__main__':
     app.run(debug=True)
