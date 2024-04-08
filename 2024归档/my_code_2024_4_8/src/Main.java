13c8b055408d75f184d4e6cacdd44241721af0a4
limou3434 <898738804@qq.com>
2024-04-08 20:17:27 +0800

    4.8.一些脚本测试和java代码测试

---
+++ 2024归档/my_code_2024_4_8/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void printSchoolSongFromFile(String filename) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            System.out.println("中山大学南方学院校歌:");
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("读取文件时出错：" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        printSchoolSongFromFile("D:\\GitWork\\limou-c-test-code\\2024归档\\my_code_2024_4_8\\src\\test.txt");
+    }
+}
